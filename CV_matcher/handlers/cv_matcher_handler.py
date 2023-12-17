4dc5102e742bbff91d20afff2b5e429dd3daa969
Artem <ardan2076@mail.ru>
2023-12-18 01:51:00 +0300

    add inference with triton server

--- CV_matcher/handlers/cv_matcher_handler.py
+++ CV_matcher/handlers/cv_matcher_handler.py
@@ -1,31 +1,38 @@
 import numpy as np
 from transformers import AutoTokenizer
-from optimum.onnxruntime import ORTModelForFeatureExtraction
-import torch
+import tritonclient.http as httpclient
 
 tokenizer = AutoTokenizer.from_pretrained(
     "/app/handlers/weights/rubert_onnx/",
     # export=True,
     provider="CUDAExecutionProvider",
 )
-model = ORTModelForFeatureExtraction.from_pretrained(
-    "/app/handlers/weights/rubert_onnx/",
-    # export=True,
-    provider="CUDAExecutionProvider",
-)
+
+client = httpclient.InferenceServerClient(url="127.0.0.1:8000")
 
 
-def embed_bert_cls(text, model, tokenizer):
-    t = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        model_output = model(**{k: v.to(model.device) for k, v in t.items()})
-    embeddings = model_output.last_hidden_state[:, 0, :]
-    embeddings = torch.nn.functional.normalize(embeddings)
-    return embeddings[0].cpu().numpy().tolist()
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
+
+
+def embed_bert_cls(text):
+    t = tokenizer(text, padding=True, truncation=True, return_tensors="np")
+    input = []
+    for i in t:
+        input_temp = httpclient.InferInput(i, t[i].shape, datatype="INT64")
+        input_temp.set_data_from_numpy(t[i], binary_data=True)
+        input.append(input_temp)
+    outputs = client.infer(model_name="feature_extractor", inputs=input)
+    out_np = outputs.as_numpy('last_hidden_state')[0, 0, :]
+    embedding = normalize(out_np)
+    return embedding.tolist()
 
 
 def get_text_vector(file_full_path):
     with open(file_full_path, "r") as f:
         text = f.read()
-    embedding = embed_bert_cls(text, model, tokenizer)
+    embedding = embed_bert_cls(text)
     return embedding
